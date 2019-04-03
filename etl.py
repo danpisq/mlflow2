f659fa89694cee3ea77037fec8b0ac94758358d6
Piskorski, Daniel <daniel.piskorski@pmi.com>
2019-04-03 16:39:01 +0200

    adding data file, and train.py

--- etl.py
+++ etl.py
@@ -15,13 +15,13 @@ def normalize_features(data_frame, target_col):
 
 	scaler = StandardScaler()
 	scaled_df = pd.DataFrame(scaler.fit_transform(features_df), columns=features_df.columns)
-	scaled_df[target_col] = data_frame['target_col']
+	scaled_df[target_col] = data_frame[target_col]
 	return scaled_df
 
 
 def transform(data_frame):
 	#normalize
-	scaled_df = normalize_features(data_frame)
+	scaled_df = normalize_features(data_frame, "quality")
 	#other transformations
 	return scaled_df
 
@@ -29,11 +29,22 @@ def transform(data_frame):
 @click.command()
 @click.option("--training_data", default="./wine-quality.csv" )
 def etl_data(training_data):
-	transformed_df = transform( pd.load_csv(training_data) )
 	with mlflow.start_run():
-		tmpdir = tempfile.mldtemp()
-		training_data_path = os.path.join(tmpdir, 'training_data.csv')
-		mlflow.log_artifacts(training_data_path, "training_data")
+		transformed_df = transform( pd.read_csv(training_data) )
 
+		mlflow.log_param("norm", "MinMaxScaler")
+		mlflow.log_metric("cols", transformed_df.shape[1])
+		mlflow.log_metric("rows", transformed_df.shape[0])
+
+		tmpdir = tempfile.mkdtemp()
+		training_data_path = os.path.join(tmpdir, 'training_data.csv')
+		transformed_df.to_csv(training_data_path)
+		mlflow.log_artifact(training_data_path)
+		
+		training_desc_path = os.path.join(tmpdir, 'training_desc.txt')
+		transformed_df.describe().to_csv(training_desc_path)
+		mlflow.log_artifact(training_desc_path)
+
+		
 if __name__ == '__main__':
 	etl_data()	
---
+++ train.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import numpy as np
+import click
+import mlflow
+import mlflow.sklearn
+
+from sklearn.linear_model import ElasticNet
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import train_test_split
+
+def eval_metrics(actual, pred):
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
+    return rmse, mae, r2
+
+@click.command()
+@click.option("--data-file", default="wine-quality.csv")
+@click.option("--alpha", default=0.1)
+@click.option("--l1-ratio", default=0.1)
+def train(data_file, alpha, l1_ratio):
+	df = pd.read_csv(data_file)
+
+	train, test = train_test_split(df)
+
+	X_train = train.drop(['quality'], axis=1)
+	X_test = test.drop(['quality'], axis=1)
+	y_train = train['quality']
+	y_test = test['quality']
+
+
+	model = ElasticNet(alpha=alpha, l1_ratio=l1_ratio)
+	model.fit(X_train, y_train)
+
+	predicted_qualities = model.predict(X_test)
+	(rmse, mae, r2) = eval_metrics(y_test, predicted_qualities)
+
+	print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
+	print("  RMSE: %s" % rmse)
+	print("  MAE: %s" % mae)
+	print("  R2: %s" % r2)
+
+	mlflow.log_param("alpha", alpha)
+	mlflow.log_param("l1_ratio", l1_ratio)
+	mlflow.log_metric("rmse", rmse)
+	mlflow.log_metric("r2", r2)
+	mlflow.log_metric("mae", mae)
+
+	mlflow.sklearn.log_model(model, "model")
+
+
+
+if __name__ == '__main__':
+	train()
